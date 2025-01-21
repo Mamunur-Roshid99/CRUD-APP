022f5b238af5bc89dc38b95a8ebd20bf637cf456
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2025-01-22 00:19:49 +0600

    Complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate = useNavigate()
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={() => {
+        navigate("/show-books", {replace: true})
+      }}>Go To Show Books</button>
     </div>
   )
 }
